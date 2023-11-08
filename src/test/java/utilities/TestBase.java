294b541471eb51a6a3404ed4d027f5bc04153456
yasin mesekoparan <y.mesekoparan1510@gmail.com>
2023-11-09 00:15:33 +0300

    08.11

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,51 @@
+package utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //  driver.quit();
+    }
+
+    //Select Visible Text DropDown
+    public void selectVisible(WebElement ddm, String option){
+        Select select = new Select(ddm);
+        select.selectByVisibleText(option);
+    }
+
+    //Select index DropDown
+    public void selectIndex(WebElement ddm, int index){
+        Select select = new Select(ddm);
+        select.selectByIndex(index);
+    }
+
+    //Hard Wait
+    public void waitForSecond(int second)  {
+        try {
+            Thread.sleep(second*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+}
